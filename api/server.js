aa8f13685904ada5f1bd1bde9962f231fc4b7ea9
Pramodh Ganeshan <pormahd@gmail.com>
2023-12-27 06:37:11 +0000

    Reorganised file structure

--- api/server.js
+++ api/server.js
@@ -1,21 +1,28 @@
+// Modules
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 
+// Database
 const connectDB = require('./db.js')
 
+// Config
 const port = 3080;
 
+// Middleware
 app.use(morgan('tiny'))
 app.use(cors())
 app.use(bodyParser.json())
 
+// Connect to DB
 connectDB()
 
+// Routes
 app.use('/api/links', require('./routes/links'))
 
+// Listen
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
