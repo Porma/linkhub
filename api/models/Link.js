3b93efd0652fe6b802e966121e9a677d3c0cf982
Pramodh Ganeshan <pormahd@gmail.com>
2023-12-28 11:18:09 +0000

    Added validation and timestamps to Link model

--- api/models/Link.js
+++ api/models/Link.js
@@ -2,8 +2,17 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema;
 
 const LinkSchema = new Schema({
-  title: String,
-  uri: String
+  title: {
+    type: String,
+    required: true
+  },
+  uri: {
+    type: String,
+    required: true
+  }
+},
+{
+  timestamps: true
 })
 
 module.exports = mongoose.model('Link', LinkSchema);
