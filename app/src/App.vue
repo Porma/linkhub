a4f2e40e633622c0764b98d3ef3e0393f5fa55e7
Pramodh Ganeshan <pormahd@gmail.com>
2024-02-15 21:53:55 +1100

    Built app link list

--- app/src/App.vue
+++ app/src/App.vue
@@ -11,7 +11,7 @@
   </nav>
 
   <div class="container">
-    <h1>LinkHub</h1>
+    <!-- <h1>LinkHub</h1> -->
 
     <LinkList />
   </div>
@@ -33,7 +33,7 @@ export default {
   /* font-family: Avenir, Helvetica, Arial, sans-serif; */
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   /* color: #2c3e50; */
   /* margin-top: 60px; */
 }
